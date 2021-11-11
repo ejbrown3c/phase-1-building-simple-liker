f182af5eac7a2381c37f5688419c5bf619321d6b
EJ Brown <ejbrown3c@gmail.com>
2021-11-10 19:48:35 -0500

    commit tree 8b42688cee847fce220a33528a55f4141770bed0

--- main.js
+++ main.js
@@ -4,7 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
 
+FULL_HEART.class = ".activated-heart"
+
+let heartbutton = document.querySelectorAll("span.like-glyph")
+
+heartbutton.forEach(heartbutton => heartbutton.addEventListener("click", handleLikeButton))
+
+function handleLikeButton() {
+  console.log(heartbutton.target)
+  mimicServerCall()
+  .then (() => {
+      if(heartbutton.target.innertext === EMPTY_HEART) {
+          heartbutton.target.innertext = FULL_HEART}
+      else if (heartbutton.target.innertext === FULL_HEART) {
+        heartbutton.target.innertext = EMPTY_HEART
+      }  
+  })
+  .catch(() => {
+    const errorMessage = document.querySelector("#modal")
+    console.log(errorMessage)
+    errorMessage.className = "show-message"
+
+  setTimeout(() => {
+    const errorMessage = document.querySelector("#modal")
+    errorMessage.className = "hidden"}, 3000)}
+  )}
+  })
 
 
 //------------------------------------------------------------------------------
